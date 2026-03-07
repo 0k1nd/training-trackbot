65081adad948e78dc0b7daf28ca8775d5c3b1e28
0k1nd <akindtyrbo@gmail.com>
2026-03-08 01:10:51 +0400

    feat(backend): register model into admin

---
+++ backend/apps/accounts/admin.py
@@ -0,0 +1,105 @@
+# apps/accounts/admin.py
+from django.contrib import admin
+
+from .models import User, BodyMetrics
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "was_born_at",
+        "registed_at",
+    )
+    list_display_links = ("username",)
+    list_filter = (
+        "gender",
+        "registed_at",
+    )
+    search_fields = (
+        "username",
+        "chat_id",
+    )
+    readonly_fields = ("registed_at",)
+    ordering = ("-registed_at",)
+    fieldsets = (
+        ("Основная информация", {
+            "fields": (
+                "username",
+                "gender",
+                "was_born_at",
+            )
+        }),
+        ("Телега / доп. поля", {
+            "fields": (
+                "chat_id",
+            )
+        }),
+        ("Системная инфа", {
+            "fields": (
+                "registed_at",
+            )
+        }),
+    )
+
+
+class BodyMetricsInline(admin.TabularInline):
+    model = BodyMetrics
+    extra = 0
+    fields = (
+        "date",
+        "weight_kg",
+        "body_fat_percent",
+        "waist_cm",
+        "hips_cm",
+        "chest_cm",
+        "biceps_cm",
+    )
+    readonly_fields = ("date",)
+    ordering = ("-date",)
+
+
+@admin.register(BodyMetrics)
+class BodyMetricsAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "date",
+        "weight_kg",
+        "body_fat_percent",
+        "waist_cm",
+        "hips_cm",
+    )
+    list_display_links = ("id", "user")
+    list_filter = ("date", "user")
+    search_fields = ("user__username", "user__chat_id")
+    readonly_fields = ("date",)
+    ordering = ("-date",)
+
+    fieldsets = (
+        ("Общая информация", {
+            "fields": (
+                "user",
+                "date",
+                "weight_kg",
+                "body_fat_percent",
+            )
+        }),
+        ("Замеры (см)", {
+            "fields": (
+                "neck_cm",
+                "chest_cm",
+                "waist_cm",
+                "hips_cm",
+                "thigh_cm",
+                "calf_cm",
+                "biceps_cm",
+            )
+        }),
+        ("Дополнительно", {
+            "fields": ("note",)
+        }),
+    )
+
+
+UserAdmin.inlines = [BodyMetricsInline]
